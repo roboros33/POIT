ff123161a44040fe7ad7a8c5d37c436f86d16aad
roboros33 <roboros33@gmail.com>
2022-05-22 20:52:56 +0200

    Create projekt.ino

---
+++ projekt.ino
@@ -0,0 +1,22 @@
+
+float in,out;
+float poz_napatie = 127;
+
+
+void setup() {
+   pinMode(3, OUTPUT);
+   Serial.begin(9600);
+}
+
+void loop() {
+	
+	in=(float)analogRead(A0)*5/1023;
+	delay(50);
+
+        analogWrite(3,poz_napatie); 
+	Serial.println(out);
+	
+	if (Serial.read() != -1) {
+		poz_napatie = Serial.read();
+	}                       
+  }
