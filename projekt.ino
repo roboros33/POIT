f8edff500cd7da41f4023c77b082b71baba1ebd6
roboros33 <roboros33@gmail.com>
2022-05-23 23:18:29 +0200

    Update projekt.ino
    final, mal by ist

--- projekt.ino
+++ projekt.ino
@@ -1,35 +1,25 @@
-float temp;
+float pwm;
 float in,out;
 float poz_napatie = 1.0;
-static char buffer[32];
-static size_t pos = 0;
+
 
 void setup() {
-   	pinMode(3, OUTPUT);
-   	Serial.begin(9600);
-   	//temp=poz_napatie/5*255;
+   pinMode(3, OUTPUT);
+   Serial.begin(9600);
 }
 
 void loop() {
-	in=(float)analogRead(A0)*5/1023;
-	delay(200);
-    	if (poz_napatie != -1 ){
-    		temp=poz_napatie/5*255;
-    	}
+	
+	  in=(float)analogRead(A0)*5/1023;
+	  delay(200);
+    pwm=poz_napatie/5*255;
     
-    	analogWrite(3,(int)temp); 
-	Serial.println(in);
-    	//Serial.println(poz_napatie);
-	  
-	while (Serial.available() > 0) {
-      		char c = Serial.read();
-        	if (c == '\n') {  
-            		float value = atof(buffer);
-            		poz_napatie = value;
-            		Serial.println(value);
-            		pos = 0;
-        	} else if (pos < sizeof buffer - 1) {  
-            	buffer[pos++] = c;
-        	}
-	}	
-}
+    analogWrite(3,(int)pwm); 
+	  Serial.println(in);
+    //Serial.println(poz_napatie);
+
+    
+      if (Serial.available() > 0) {
+        poz_napatie = Serial.readString().toFloat();
+      }                     
+  }
